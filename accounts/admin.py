d6178fb6d66059018eb90c6484c82b543d6a81b3
mostafanasser2000 <mustafanasser503@gmail.com>
2024-11-03 16:29:07 +0200

    fix: remove usable_password field from default add_fieldsets

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,7 +11,15 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ["email", "username", "name", "is_staff"]
     fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    ) + ((None, {"fields": ("name",)}),)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
