f1c9b1d63e6139fd71045084f43051ebb8d51098
mostafanasser2000 <mustafanasser503@gmail.com>
2024-11-05 16:42:06 +0200

    refactor: :recycle: Replace some views and urls with viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,19 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
-class PostList(ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthorOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthorOrReadOnly]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
